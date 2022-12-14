5c6783d171b629f0f089f515751074d8a50cfd95
Felix Dürrwald <mplabs@mplabs.de>
2022-12-14 11:20:43 +0100

    Day 14

---
+++ src/utils/number.ts
@@ -0,0 +1,13 @@
+export function clamp(number: number, boundOne: number, boundTwo?: number): number {
+  if (!boundTwo) {
+    return Math.max(number, boundOne) === boundOne ? number : boundOne
+  } else if (Math.min(number, boundOne) === number) {
+    return boundOne
+  } else if (Math.max(number, boundTwo) === number) {
+    return boundTwo
+  }
+  return number
+}
+
+export const inRange = (num: number, a: number, b: number = 0): boolean =>
+  Math.min(a, b) <= num && num < Math.max(a, b)
